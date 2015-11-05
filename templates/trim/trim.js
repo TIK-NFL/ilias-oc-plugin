13f5094db7d2b91e89a3cfb1dbea45e00e9c5a8f
Per Pascal Grube <pascal.grube@flightmare.net>
2015-11-05 19:29:11 +0100

    Added play from trimin and play to trimout functions

--- templates/trim/trim.js
+++ templates/trim/trim.js
@@ -2,13 +2,31 @@
 $(document).ready(function () {
   var myPlayer = videojs('mhpreviewvideo');
   $('#settrimin').click(function(){
-      $('#trimin').val(convertTime(myPlayer.currentTime().toFixed(0)));
+      $('#trimin').val(convertSecToTime(myPlayer.currentTime().toFixed(0)));
       $( "#slider-range" ).slider("values",0,myPlayer.currentTime().toFixed(0));
   });
   $('#settrimout').click(function(){
-      $('#trimout').val(convertTime(myPlayer.currentTime().toFixed(0)));
+      $('#trimout').val(convertSecToTime(myPlayer.currentTime().toFixed(0)));
       $( "#slider-range" ).slider("values",1,myPlayer.currentTime().toFixed(0));
   });  
+  $('#playtrimin').click(function(){
+      time = convertTimeToSec($('#trimin').val());
+      myPlayer.currentTime(time);
+      myPlayer.play();
+  });  
+  $('#playtrimout').click(function(){
+      time = convertTimeToSec($('#trimout').val()) - 10;
+      pausetime = 11000;
+      if(time < 0) { 
+        pausetime = (11+time)*1000;
+        time = 0;        
+      } 
+      myPlayer.currentTime(time);
+      myPlayer.play();
+      myPlayer.setTimeout(myPlayer.pause,pausetime )
+      
+  });  
+
   $("#lefttrackflavor option").filter(function() {
     return $(this).val() == $('#lefttrackflavor').data('preset'); 
   }).prop('selected', true);
@@ -50,14 +68,20 @@ $(document).ready(function () {
 
 function refreshTimes() {
     var values = $( "#slider-range" ).slider( "values" );   
-    $('#trimin').val(convertTime(values[0]));    
-    $('#trimout').val(convertTime(values[1]));
+    $('#trimin').val(convertSecToTime(values[0]));    
+    $('#trimout').val(convertSecToTime(values[1]));
   }
 
-function convertTime(totalSec){
+function convertSecToTime(totalSec){
   var hours = parseInt( totalSec / 3600 ) % 24;
   var minutes = parseInt( totalSec / 60 ) % 60;
   var seconds = totalSec % 60;
   var result = hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
   return result;
+}
+
+function convertTimeToSec(time){
+  var ts = time.split(':'); // split it at the colons
+  var seconds = (+ts[0]) * 60 * 60 + (+ts[1]) * 60 + (+ts[2]); 
+  return seconds;
 }
