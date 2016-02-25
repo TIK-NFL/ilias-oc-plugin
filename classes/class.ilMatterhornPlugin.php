21f11aab4661dc912343882c740d1560fc50d0ad
Per Pascal Grube <pascal.grube@flightmare.net>
2016-02-25 10:01:14 +0100

    Updated supported version to 5.1

--- classes/class.ilMatterhornPlugin.php
+++ classes/class.ilMatterhornPlugin.php
@@ -15,5 +15,17 @@ class ilMatterhornPlugin extends ilRepositoryObjectPlugin
 	{
 		return "Matterhorn";
 	}
+	protected function uninstallCustom()
+        {
+                /**
+                 * @var $ilDB ilDB
+                 */
+                global $ilDB;
+                $ilDB->query('DROP TABLE  rep_robj_xmh_config, 
+                                          rep_robj_xmh_data,
+                                          rep_robj_xmh_rel_ep, 
+                                          rep_robj_xmh_slidetext');
+        }
+
 }
 ?>
