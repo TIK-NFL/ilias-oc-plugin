a6e3c177477fa33748b4d38cc173a8a22ba7b1ec
Per Pascal Grube <pascal.grube@flightmare.net>
2017-02-06 12:34:07 +0100

    Plugin now requires 5.2

--- plugin.php
+++ plugin.php
@@ -4,11 +4,11 @@
 $id = "xmh";
  
 // code version; must be changed for all code changes
-$version = "0.2.2";
+$version = "0.3.0";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "4.4.0";
+$ilias_min_version = "5.2.0";
 $ilias_max_version = "5.2.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
