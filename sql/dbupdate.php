e268150dcb3cee3148036d11274a1eac587de07c
Legion2 <leon.k97@gmx.de>
2017-07-21 16:05:33 +0200

    applied PSR-2 to dbupdate.php

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,133 +1,141 @@
 <#1>
 <?php
 $fields = array(
-	'id' => array(
-		'type' => 'integer',
-		'length' => 4,
-		'notnull' => true
-	),
-	'is_online' => array(
-		'type' => 'integer',
-		'length' => 1,
-		'notnull' => false
-	),
-	'series' => array(
-		'type' => 'text',
-		'length' => 1000,
-		'fixed' => false,
-		'notnull' => false
-	),
-	'mhretval' => array(
-		'type' => 'text',
-		'length' => 10,
-		'fixed' => false,
-		'notnull' => false
-	)
+    'id' => array(
+        'type' => 'integer',
+        'length' => 4,
+        'notnull' => true
+    ),
+    'is_online' => array(
+        'type' => 'integer',
+        'length' => 1,
+        'notnull' => false
+    ),
+    'series' => array(
+        'type' => 'text',
+        'length' => 1000,
+        'fixed' => false,
+        'notnull' => false
+    ),
+    'mhretval' => array(
+        'type' => 'text',
+        'length' => 10,
+        'fixed' => false,
+        'notnull' => false
+    )
 );
 
 $ilDB->createTable("rep_robj_xmh_data", $fields);
-$ilDB->addPrimaryKey("rep_robj_xmh_data", array("id"));
+$ilDB->addPrimaryKey("rep_robj_xmh_data", array(
+    "id"
+));
 ?>
 <#2>
 <?php
 $fields = array(
-		'cfgkey' => array(
-				'type' => 'text',
-				'length' => 20,
-				'notnull' => true
-		),
-		'cfgvalue' => array(
-				'type' => 'text',
-				'length' => 100,
-				'fixed' => false,
-				'notnull' => false
-		)
+    'cfgkey' => array(
+        'type' => 'text',
+        'length' => 20,
+        'notnull' => true
+    ),
+    'cfgvalue' => array(
+        'type' => 'text',
+        'length' => 100,
+        'fixed' => false,
+        'notnull' => false
+    )
 );
 
 $ilDB->createTable("rep_robj_xmh_config", $fields);
-$ilDB->addPrimaryKey("rep_robj_xmh_config", array("cfgkey"));
+$ilDB->addPrimaryKey("rep_robj_xmh_config", array(
+    "cfgkey"
+));
 ?>
 <#3>
 <?php
-	$ilDB->addTableColumn('rep_robj_xmh_data', 'lectureid', array(
-		'type' => 'text',
-		'length' => 20,
-		'fixed' => false,
-		'notnull' => false
+$ilDB->addTableColumn('rep_robj_xmh_data', 'lectureid', array(
+    'type' => 'text',
+    'length' => 20,
+    'fixed' => false,
+    'notnull' => false
 ));
 ?>
 <#4>
 <#5>
 <?php
-	$ilDB->addTableColumn('rep_robj_xmh_data', 'viewmode', array(
-		'type' => 'integer',
-		'length' => 1,
-		'notnull' => false,
-	        'default' => 0
+$ilDB->addTableColumn('rep_robj_xmh_data', 'viewmode', array(
+    'type' => 'integer',
+    'length' => 1,
+    'notnull' => false,
+    'default' => 0
 ));
 ?>
 <#6>
 <?php
-  $ilDB->addTableColumn('rep_robj_xmh_data', 'manualrelease', array(
-          'type' => 'integer',
-          'length' => 1,
-          'notnull' => false,
-          'default' => 0
+$ilDB->addTableColumn('rep_robj_xmh_data', 'manualrelease', array(
+    'type' => 'integer',
+    'length' => 1,
+    'notnull' => false,
+    'default' => 0
 ));
 ?>
 <#7>
 <?php
 $fields = array(
-                'episode_id' => array(
-                                'type' => 'text',
-                                'length' => 50,
-                                'fixed' => false,
-                                'notnull' => true
-                ),
-                'series_id' => array(
-                                'type' => 'text',
-                                'length' => 150,
-                                'fixed' => false,
-                                'notnull' => true
-                )
+    'episode_id' => array(
+        'type' => 'text',
+        'length' => 50,
+        'fixed' => false,
+        'notnull' => true
+    ),
+    'series_id' => array(
+        'type' => 'text',
+        'length' => 150,
+        'fixed' => false,
+        'notnull' => true
+    )
 );
 
 $ilDB->createTable("rep_robj_xmh_rel_ep", $fields);
-$ilDB->addPrimaryKey("rep_robj_xmh_rel_ep", array("episode_id"));
-$ilDB->addIndex("rep_robj_xmh_rel_ep", array("series_id"),'ser');
+$ilDB->addPrimaryKey("rep_robj_xmh_rel_ep", array(
+    "episode_id"
+));
+$ilDB->addIndex("rep_robj_xmh_rel_ep", array(
+    "series_id"
+), 'ser');
 ?>
 <#8>
 <?php
-  $ilDB->addTableColumn('rep_robj_xmh_data', 'fsinodupdate', array(
-          'type' => 'integer',
-          'length' => 8,
-          'notnull' => false,
-          'default' => 0
+$ilDB->addTableColumn('rep_robj_xmh_data', 'fsinodupdate', array(
+    'type' => 'integer',
+    'length' => 8,
+    'notnull' => false,
+    'default' => 0
 ));
 ?>
 <#9>
 <?php
 
-  $ilDB->modifyTableColumn('rep_robj_xmh_data', 'manualrelease', array(
-          'type' => 'integer',
-          'length' => 1,
-          'notnull' => true,
-          'default' => 0
+$ilDB->modifyTableColumn('rep_robj_xmh_data', 'manualrelease', array(
+    'type' => 'integer',
+    'length' => 1,
+    'notnull' => true,
+    'default' => 0
 ));
-  $ilDB->modifyTableColumn('rep_robj_xmh_data', 'fsinodupdate', array(
-          'type' => 'integer',
-          'length' => 8,
-          'notnull' => true,
-          'default' => 0
+$ilDB->modifyTableColumn('rep_robj_xmh_data', 'fsinodupdate', array(
+    'type' => 'integer',
+    'length' => 8,
+    'notnull' => true,
+    'default' => 0
 ));
 ?>
 <#10>
 <?php
-  $ilDB->addTableColumn('rep_robj_xmh_data', 'download', array(
-          'type' => 'integer',
-          'length' => 8,
-          'notnull' => false,
-          'default' => 0
+$ilDB->addTableColumn('rep_robj_xmh_data', 'download', array(
+    'type' => 'integer',
+    'length' => 8,
+    'notnull' => false,
+    'default' => 0
 ));
 ?>
 <#11>
@@ -138,70 +146,72 @@ $ilDB->renameTableColumn('rep_robj_xmh_data', 'id', 'obj_id');
 <#13>
 <?php
 $fields = array(
-                'episode_id' => array(
-                                'type' => 'text',
-                                'length' => 50,
-                                'fixed' => false,
-                                'notnull' => true
-                ),
-				'series_id' => array(
-						'type' => 'integer',
-						'length' => 8,
-						'notnull' => true
-				),
-				'slidetime' => array(
-						'type' => 'text',
-						'length' => 100,
-						'fixed' => false,
-						'notnull' => true
-				),
-                'slidetext' => array(
-                                'type' => 'text',
-                                'length' => 4000,
-                                'fixed' => false,
-                                'notnull' => true
-                )
+    'episode_id' => array(
+        'type' => 'text',
+        'length' => 50,
+        'fixed' => false,
+        'notnull' => true
+    ),
+    'series_id' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'slidetime' => array(
+        'type' => 'text',
+        'length' => 100,
+        'fixed' => false,
+        'notnull' => true
+    ),
+    'slidetext' => array(
+        'type' => 'text',
+        'length' => 4000,
+        'fixed' => false,
+        'notnull' => true
+    )
 );
 
 $ilDB->createTable("rep_robj_xmh_slidetext", $fields);
-$ilDB->addPrimaryKey("rep_robj_xmh_slidetext",array("episode_id","slidetime"));
+$ilDB->addPrimaryKey("rep_robj_xmh_slidetext", array(
+    "episode_id",
+    "slidetime"
+));
 ?>
 
 <#14>
 <?php
 $fields = array(
-                'id' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-
-                'series_id' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-                'episode_id' => array(
-                                'type' => 'text',
-                                'length' => 50,
-                                'fixed' => false,
-                                'notnull' => true
-                ),
-                'user_id' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-                'intime' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-                'outtime' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
+    'id' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'series_id' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'episode_id' => array(
+        'type' => 'text',
+        'length' => 50,
+        'fixed' => false,
+        'notnull' => true
+    ),
+    'user_id' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'intime' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'outtime' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    )
 );
 
 $ilDB->createTable("rep_robj_xmh_usrtrack", $fields);
@@ -212,32 +222,32 @@ $ilDB->manipulate(" ALTER TABLE rep_robj_xmh_usrtrack MODIFY COLUMN `id` BIGINT 
 <#15>
 <?php
 $fields = array(
-                'id' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-                'user_id' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-                'episode_id' => array(
-                                'type' => 'text',
-                                'length' => 50,
-                                'fixed' => false,
-                                'notnull' => true
-                ),
-                'intime' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
-                'outtime' => array(
-                                'type' => 'integer',
-                                'length' => 8,
-                                'notnull' => true
-                ),
+    'id' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'user_id' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'episode_id' => array(
+        'type' => 'text',
+        'length' => 50,
+        'fixed' => false,
+        'notnull' => true
+    ),
+    'intime' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    ),
+    'outtime' => array(
+        'type' => 'integer',
+        'length' => 8,
+        'notnull' => true
+    )
 );
 
 $ilDB->createTable("rep_robj_xmh_views", $fields);
