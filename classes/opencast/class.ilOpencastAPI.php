f3e3951c8f9f113decea1211c3892dd5f44acf25
Legion2 <leon.k97@gmx.de>
2019-01-08 14:36:34 +0100

    added "v" to opencast version

--- classes/opencast/class.ilOpencastAPI.php
+++ classes/opencast/class.ilOpencastAPI.php
@@ -9,7 +9,7 @@ ilPlugin::getPluginObject(IL_COMP_SERVICE, 'Repository', 'robj', 'Matterhorn')->
 class ilOpencastAPI
 {
 
-    const API_VERSION = "1.0.0";
+    const API_VERSION = "v1.0.0";
 
     private static $instance = null;
 
