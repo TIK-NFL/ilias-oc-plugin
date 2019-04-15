0fbf80c93c322ad996d7f4d59af94bdd350a2d45
Legion2 <leon.k97@gmx.de>
2019-04-15 13:16:06 +0200

    fixed typo

--- classes/class.ilMatterhornConfig.php
+++ classes/class.ilMatterhornConfig.php
@@ -227,7 +227,7 @@ class ilMatterhornConfig
      */
     public function setPublisher(string $publisher)
     {
-        $this->set('publisher', $publisher);
+        $this->setValue('publisher', $publisher);
     }
 
     /**
